e4af6bf553902d88aa2b50ac29c67af4b1b5accd
Xian <xian.lopez@davantis.com>
2019-04-09 19:07:26 +0200

    More working...

--- CreateDatabase.py
+++ CreateDatabase.py
@@ -2,16 +2,70 @@ import connection
 
 conn, cursor = connection.connect()
 
-cursor.execute("CREATE TABLE EXECUTIONS (ExecId int, ExpId int, WorkerId int, Success bit, ResultsId int, "
-               "ErrorCode int, ExecTime_s float, DateStart datetime, AssignmentId int)")
-cursor.execute("CREATE TABLE EXPERIMENTS (ExpId int, ConfigPath varchar(255), Finished bit, "
-               "Assigned bit, FinalExecId int)")
-cursor.execute("CREATE TABLE ASSIGNMENTS (AssignmentId int, ExpId int, WorkerId int, InProgress bit, "
-               "Completed bit, AssignmentDate datetime, TimedOut bit, Forgotten bit)")
-cursor.execute("CREATE TABLE RESULTS (ExecId int, BestEpoch int, Loss float, Accuracy float, IoU float, mAP float)")
-cursor.execute("CREATE TABLE REGISTERED_WORKERS (WorkerId int, WorkerName varchar(255), GPUName varchar(255), "
-               "GPUMemory varchar(255))")
-cursor.execute("CREATE TABLE ONLINE_WORKERS (WorkerId int, LastHeartbit datetime)")
+cursor.execute("CREATE TABLE EXECUTIONS ("
+               "ExecId int NOT NULL PRIMARY KEY, "
+               "ExpId int NOT NULL, "
+               "WorkerId int NOT NULL, "
+               "Success bit, "
+               "ResultsId int, "
+               "ErrorCode int, "
+               "ExecTime_s float, "
+               "DateStart datetime, "
+               "AssignmentId int NOT NULL"
+               ")")
+
+cursor.execute("CREATE TABLE EXPERIMENTS ("
+               "ExpId int NOT NULL PRIMARY KEY, "
+               "ConfigPath varchar(255) NOT NULL, "
+               "Finished bit NOT NULL, "
+               "Assigned bit NOT NULL, "
+               "FinalExecId int, "
+               "Priority int NOT NULL, "
+               "InsufficientMemory_GB int"
+               ")")
+
+cursor.execute("CREATE TABLE ASSIGNMENTS ("
+               "AssignmentId int NOT NULL PRIMARY KEY, "
+               "ExpId int NOT NULL, "
+               "WorkerId int NOT NULL, "
+               "InProgress bit NOT NULL, "
+               "Completed bit NOT NULL, "
+               "AssignmentDate datetime NOT NULL, "
+               "TimedOut bit, "
+               "Forgotten bit"
+               ")")
+
+cursor.execute("CREATE TABLE RESULTS ("
+               "ResultsId int NOT NULL PRIMARY KEY, "
+               "ExecId int NOT NULL, "
+               "BestEpoch int, "
+               "Loss float, "
+               "Accuracy float, "
+               "IoU float, "
+               "mAP float)")
+
+cursor.execute("CREATE TABLE REGISTERED_WORKERS ("
+               "WorkerId int NOT NULL PRIMARY KEY, "
+               "WorkerName varchar(255) NOT NULL, "
+               "GPUName varchar(255) NOT NULL, "
+               "GPUMemory varchar(255) NOT NULL"
+               ")")
+
+cursor.execute("CREATE TABLE ONLINE_WORKERS ("
+               "WorkerId int NOT NULL, "
+               "LastHeartbit datetime"
+               ")")
+
+cursor.execute("CREATE TABLE VALID_WORKERS ("
+               "ExpId int NOT NULL, "
+               "WorkerId int NOT NULL, "
+               "Valid bit"
+               ")")
+cursor.execute("CREATE TABLE SETTINGS ("
+               "Name varchar(255) NOT NULL, "
+               "Value varchar(255) NOT NULL"
+               ")")
+
 conn.commit()
 
 
--- Director.py
+++ Director.py
@@ -6,6 +6,7 @@ import datetime
 loopPeriod_s = 30
 maxTaskTime_h = 72
 maxAssignmentWaitingTime_h = 24
+EC_OOM = 1
 
 
 class Director:
@@ -16,17 +17,14 @@ class Director:
         while True:
             start = time.time()
 
-            # Assign new tasks to workers.
-            self.AssignNewTasksToWorkers()
-
             # Check finished tasks.
             self.CheckFinishedTasks()
 
-            # Check timed-out tasks.
-            self.CheckTimedoutTasks()
+            # Check assignments with off-line workers.
+            self.CheckAssignmentsWithOfflineWorkers()
 
-            # Check forgotten tasks.
-            self.CheckForgottenTasks()
+            # Assign new tasks to workers.
+            self.AssignNewTasksToWorkers()
 
             # Wait for new loop.
             end = time.time()
@@ -36,46 +34,106 @@ class Director:
 
     def AssignNewTasksToWorkers(self):
         self.cursor.execute('select * from EXPERIMENTS where Finished=0 and Assigned=0')
-        query = self.cursor.fetchall()
+        query_exp = self.cursor.fetchall()
+        columns_exp = tools.get_columns_map(self.cursor)
+        for exp in query_exp:
+            memory = exp[columns_exp['InsufficientMemory_GB']]
+            if memory is None:
+                memory = 0
+            valid_workers, queues = self.GetValidOnlineWorkersWithQueue(memory)
 
     def CheckFinishedTasks(self):
-        self.cursor.execute('select * from EXECUTIONS as exec '
-                            'join EXPERIMENTS as exp on exec.IdExp = exp.IdExp '
-                            'join ASSIGNMENTS as asg on exec.IdExp = asg.IdExp '
-                            'where asg.Completed=1 and asg.TimedOut=0 and asg.Forgotten=0')
+        self.cursor.execute('select * from EXECUTIONS where Processed=0')
         query = self.cursor.fetchall()
         columns = tools.get_columns_map(self.cursor)
         for row in query:
             if row[columns['exec.Success']] == 1:
+                # Successful execution.
                 self.cursor.execute('update EXPERIMENTS set Finished=1, FinalExecId=' + str(row[columns['exec.ExecId']])
                                     + ' where ExpId=' + str(row[columns['exec.ExpId']]))
+                self.conn.commit()
             else:
-                # TODO: Reassign or give up
-                pass
+                if row[columns['exec.ErrorCode']] == EC_OOM:
+                    # Out Of Memory.
+                    # Mark this amount of memory as insufficient in the experiment:
+                    currentMemory = self.GetMemoryOfWorker(row[columns['exec.WorkerId']])
+                    self.cursor.execute('update EXPERIMENTS set InsufficientMemory_GB=' + str(currentMemory) +
+                                        ' where ExpId=' + str(row[columns['ExpId']]))
+                    self.conn.commit()
+                    # Check if there are workers with more memory:
+                    self.cursor.execute('select WorkerId from REGISTERED_WORKERS where GPUMemory>' + str(currentMemory))
+                    query_workers = self.cursor.fetchall()
+                    if len(query_workers) > 0:
+                        # There are online workers with more memory.
+                        # Mark the experiment as not assigned.
+                        self.cursor.execute('update EXPERIMENTS set Assigned=0 where ExpId=' +
+                                            str(row[columns['ExpId']]))
+                        self.conn.commit()
+                    else:
+                        # There aren't. Mark as failure.
+                        self.cursor.execute('update EXPERIMENTS set Finished=1, FinalExecId=' + str(row[columns['exec.ExecId']])
+                                            + ' where ExpId=' + str(row[columns['exec.ExpId']]))
+                        self.conn.commit()
+                else:
+                    # Other error. Mark as failure.
+                    self.cursor.execute('update EXPERIMENTS set Finished=1, FinalExecId=' + str(row[columns['exec.ExecId']])
+                                        + ' where ExpId=' + str(row[columns['exec.ExpId']]))
+                    self.conn.commit()
 
-    def CheckTimedoutTasks(self):
-        now = datetime.datetime.now()
-        self.cursor.execute('select * from ASSIGNMENTS where InProgress=1 and Completed=0 and TimedOut=0')
+            # Finally, mark the execution as processed, in order not to process it again:
+            self.cursor.execute('update EXECUTIONS set Processed=1 where ExecId=' + str(row[columns['exec.ExecId']]))
+            self.conn.commit()
+
+    def GetMemoryOfWorker(self, workerId):
+        self.cursor.execute('select GPUMemory from REGISTERED_WORKERS where WorkerId=' + str(workerId))
+        query = self.cursor.fetchall()
+        return query[0][0]
+
+    def GetValidOnlineWorkersWithQueue(self, memory):
+        # self.cursor.execute('select * from ONLINE_WORKERS as ow join REGISTERED_WORKERS as rw '
+        #                     'on ow.WorkerId=rw.WorkerId where rw.GPUMemory>' + str(memory))
+        self.cursor.execute('select * from ONLINE_WORKERS as ow '
+                            'join REGISTERED_WORKERS as rw on ow.WorkerId=rw.WorkerId '
+                            'join ASSIGNMENTS as asg on ow.WorkerId=asg.WorkerId '
+                            'where asg.Finished=0 and asg.Discarded=0 rw.GPUMemory>' + str(memory))
         query = self.cursor.fetchall()
         columns = tools.get_columns_map(self.cursor)
+        valid_workers = []
+        queues = {}
         for row in query:
-            timedelta = now - row[columns['AssignmentDate']]
-            if timedelta > datetime.timedelta(hours=maxTaskTime_h):
-                self.cursor.execute('update ASSIGNMENTS set TimedOut=1 where IdAssignment=' + str(row[columns['AssignmentId']]))
-                self.conn.commit()
-                self.ReassignExperiment(row[columns['ExpId']])
+            workerId = row[columns['ow.WorkerId']]
+            if workerId in valid_workers:
+                queues[workerId] += 1
+            else:
+                valid_workers.append(workerId)
+                queues[workerId] = 1
+        return valid_workers, queues
 
-    def CheckForgottenTasks(self):
-        now = datetime.datetime.now()
-        self.cursor.execute('select * from ASSIGNMENTS where InProgress=0 and Completed=0 and Forgotten=0')
-        query = self.cursor.fetchall()
-        columns = tools.get_columns_map(self.cursor)
-        for row in query:
-            timedelta = now - row[columns['AssignmentDate']]
-            if timedelta > datetime.timedelta(hours=maxAssignmentWaitingTime_h):
-                self.cursor.execute('update ASSIGNMENTS set Forgotten=1 where IdAssignment=' + str(row[columns['AssignmentId']]))
-                self.conn.commit()
-                self.ReassignExperiment(row[columns['ExpId']])
+    def MarkExperimentAsFailure(self, expId, execId):
+        self.cursor.execute('update EXPERIMENTS set Finished=1, FinalExecId=' + str(execId)
+                            + ' where ExpId=' + str(expId))
+        self.conn.commit()
 
-    def ReassignExperiment(self, expId):
-        pass
+    def CheckAssignmentsWithOfflineWorkers(self):
+        self.cursor.execute('select asg.* from ASSIGNMENTS where InProgress=1 and Finished=0 and Discarded=0')
+        query_asg = self.cursor.fetchall()
+        columns_asg = tools.get_columns_map(self.cursor)
+        self.cursor.execute('select * from ONLINE_WORKERS')
+        query_ow = self.cursor.fetchall()
+        columns_ow = tools.get_columns_map(self.cursor)
+        for asg in query_asg:
+            workerId = asg[columns_asg['WorkerId']]
+            foundOnlineWorker = False
+            for ow in query_ow:
+                if ow[columns_ow['WorkerId']] == workerId:
+                    foundOnlineWorker = True
+                    break
+            if not foundOnlineWorker:
+                # De-assign.
+                self.cursor.execute('update ASSIGNMENTS set InProgress=0, Discarded=1 where AssignmentId=' +
+                                    str(asg[columns_asg['AssignmentId']]))
+                self.cursor.execute('update EXPERIMENTS set Assigned=0 where ExpId=' + str(asg[columns_asg['ExpId']]))
+                self.conn.commit()
+
+
+
